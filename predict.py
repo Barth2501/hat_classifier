ab439073c3a84f54c56d0c65db1d850bd1509fa8
barth2501 <lancelot2501>
2020-07-09 16:42:43 +0200

    fix issue

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from absl import app, flags, logging
-from models.resnet_50 import ResNet_50
+from .models.resnet_50 import ResNet_50
 
 def main(argv):
     model = ResNet_50()
@@ -14,6 +14,14 @@ def main(argv):
     image = tf.cast(img, tf.float32) / 256
     
     print(model.predict(image))
+
+def predict(image):
+    model = ResNet_50()
+    model.load_weights('./hat_classifier/saved_model/resnet_hub_9661/resnet_hub_000500_bs8.tf').expect_partial()
+    img = tf.expand_dims(image, 0)
+    img = tf.image.resize(img,(224,224))
+    image = tf.cast(img, tf.float32) / 256
+    return model.predict(image)
 
 if __name__ == "__main__":
     FLAGS = flags.FLAGS
